1f90e5555e8089bd3b7758c6d3d07d729682dfbe
Manaciaa <150796156+Manaciaa@users.noreply.github.com>
2024-02-13 10:05:35 +0100

    Add files via upload

---
+++ Code/Core/Interactions/interaction.js
@@ -0,0 +1,7 @@
+// Programmation d'un fichier Javascript avec les éléments suivants:
+// - 10 Variables
+// - 2 Classes et 2 Functions différentes
+// - 2 messages sur la console 
+
+
+
---
+++ Code/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+    class concrete 
+{
+    public $headtitle;
+    public $headfavicon;
+
+    public function_construct($headtitle)   
+}
+
+
+</body>
+</html><
